1fc9b82c78cc74ea09b99688b6a54bca047c9fdb
Aark Koduru <aark@Aarks-MacBook-Pro.local>
2019-04-16 13:41:29 -0700

    change

--- JonesRyan.c
+++ JonesRyan.c
@@ -42,51 +42,84 @@ int print_decimal_integer(digit *x, int n)
 {
   int i ;
   for (i = n-1 ; i >= 0 ; i--) {
-    printf("%c",x[i] + '0');
+    printf("%c",x[i]+'0') ;
   }
 }
 
 
 
-int add (digit *res,
+int mult (digit *res,
 	 digit *a, int m,
 	 digit *b, int n)
 {
-  // 'a' and 'b' are my numbers to add
+  // 'a' and 'b' are my numbers to mult
   // 'm' and 'n' are the respective lengths of 'a' and 'b'
 
+  /*
   int i,max ;
-
-  int j;
-  unsigned long temp;
 
   // i is the length of the sum`1
 
   digit ad,bd,sum,carry ;
 
-  // if (m > n) { max = m; } else { max = n; }
+  if (m > n) { max = m ; } else { max = n ; }
   carry = 0 ;
 
-  for (i = 0 ; i < (m + n - 1); i++) {
-    //if (i >= m) { ad = 0 ; } else { ad = a[i] ; }
-    //if (i >= n) { bd = 0 ; } else { bd = b[i] ; }
-    for(j = 0; j < n; j++) {
-      temp += (a[m] - '0') * (b[n] - '0');
+  for (i = 0 ; i < max ; i++) {
+    if (i >= m) { ad = 0 ; } else { ad = a[i] ; }
+    if (i >= n) { bd = 0 ; } else { bd = b[i] ; }
+
+    sum = ad + bd + carry ;
+    // what is the biggest sum can be?
+
+    if (sum >= 10) {
+      sum = sum - 10 ;
+      carry = 1 ;
+    } else {
+      carry = 0 ;
     }
 
-    res[i] = temp % 10 + '0';           // temp % 10 is the one's place
-    temp = temp / 10;             // temp / 10 is the ten's place
-    j = i + 1;
+    res[i] = sum ;
+  }
 
-    while(temp > 0) {
-      res[j] += temp % 10;
-      temp = temp / 10;
-      j++;
+  if (carry == 1) {
+    res[i] = carry ;
+    i++ ;
+  }
+
+  return i ;
+  */
+
+  // prepare arrays a and b for calculation w/ "- '0'" trick from class
+  for(int i = 0; i < m; i++) {
+    a[i] -= '0';
+  }
+
+  for(int i = 0; i < n; i++) {
+    b[i] -= '0';
+  }
+
+  // note: the largest # from two digits of length 1 == 81 (9 * 9)
+  for(int i = m - 1; i >= 0; i--) {
+    for(int j = n - 1; j >= 0; j--) {
+      res[i + j + 1] += (a[i] * b[j]);
+      res[i + j]     += res[i + j + 1] / 10; // catch the "ones" place
+      res[i + j + 1] =  res[i + j + 1] % 10; // catch the "tens" place
     }
   }
 
+  // turn res array into the approrpiate value
+  for(int i = 0; i < (n + m + 1); i++) {
+    res[i] += '0';
+  }
 
-  return m + n;
+  // terminating character at end of array (little-endian, so this is one element past the most significant digit)
+  res[n + m] = '\0';
+  printf("Result = %s\n", res);
+
+  // the add function returned the length of the sum, my code does the same. When multipling two digits of lengths n and m, the maximum length of the product is no more than (n + m)
+  // for example, if n = 3 and m = 4, n * m will have a length of no more than 7
+  return n + m;
 }
 
 
@@ -102,7 +135,7 @@ int main()
   ylen = input_decimal_integer(y) ;
   printf("y = ") ; print_decimal_integer(y,ylen) ;  printf("\n") ;
 
-  zlen = add(z,   x,xlen,    y,ylen) ;
+  zlen = mult(z, x, xlen, y, ylen);
 
   printf("z = ") ; print_decimal_integer(z,zlen) ;  printf("\n") ;
 
